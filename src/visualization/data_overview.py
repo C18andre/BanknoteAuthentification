93a4c079788cc733ee7697c0741d2408560ea18c
unknown <alexandre.giraud18@gmail.com>
2021-12-15 21:13:37 +0100

    small modifications

--- src/visualization/data_overview.py
+++ src/visualization/data_overview.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 def numeric_features_distribution(data,features,bandwith=1):
     """
     Use kde algorithm to plot the distribution of the numerical features
+
+    data: dataframe
+    bandwith: spread of kernel function
     """
     cols = 2
     rows = len(features)//2 +1
@@ -13,10 +16,29 @@ def numeric_features_distribution(data,features,bandwith=1):
         plt.subplot(rows,cols,i+1)
         plt.title(f)
         sns.kdeplot(data[f],bw=bandwith)
+
+def categorical_features_histograms(data,features):
+    """
+    Use kde algorithm to plot the distribution of the numerical features
+
+    data: dataframe
+    """
+    cols = 2
+    rows = len(features)//2 +1
+    plt.figure(figsize=(12,10))
+    plt.subplots_adjust(right=1, top=1)
+    for i,f in enumerate(features):
+        plt.subplot(rows,cols,i+1)
+        plt.title(f)
+        sns.countplot(x=f,data=data)
         
 def numeric_features_distribution_per_class(data,features,target_column,bandwith=1):
     """
     Use kde algorithm to plot the distribution of the numerical features
+
+    data: dataframe
+    target_column: name of the targeted column
+    bandwith: spread of kernel function
     """
     classes = data[target_column].unique()
     
@@ -36,3 +58,4 @@ def numeric_features_distribution_per_class(data,features,target_column,bandwith
         for c,df in tmp_df.items():
             sns.kdeplot(df[f],bw=bandwith,label=c)
         plt.legend()
+
