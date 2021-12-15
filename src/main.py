0a16a4e6cd573ce1f697fd613e1784eb889eaac4
Clément <clementdarthe@gmail.com>
2021-12-15 12:40:05 +0100

    Labelizer

--- src/main.py
+++ src/main.py
@@ -7,10 +7,18 @@ with open("src/settings.json") as file:
 import pandas as pd
 from preprocessing.cleaner import convert_cat_to_num,missing_value
 from preprocessing.test import test as p_test
-
+from preprocessing.label import label_encoder
 
 
 ############################################ Pipeline ############################################
+# Preprocessing
 data = pd.read_csv(settings["paths"]["banknote"])
 clean_data = missing_value(data)
 p_test(clean_data,data)
+label_data,label_dico = label_encoder(data)
+
+
+# Models
+
+
+# Visualisation
--- src/preprocessing/label.py
+++ src/preprocessing/label.py
@@ -1,13 +1,16 @@
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
 from collections import defaultdict
 
 def label_encoder(data) :
+    """
+    data : Data to label
+    Return labellized data and the corresponding dictionnary
+    """
     columns_to_label = data.select_dtypes(include="object").columns
-    dico_le = defaultdict(OneHotEncoder)
+    dico_le = defaultdict(LabelEncoder)
     if len(columns_to_label) == 0 :
-        return data
+        return data,{}
     for col in columns_to_label :
-        le = OneHotEncoder()
-        fit =
-    data.apply(lambda x : )
+        data[col] = dico_le[col].fit_transform(data[col])
+    return data,dico_le
 
