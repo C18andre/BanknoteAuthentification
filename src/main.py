3c7fc4520a66ee923e536775aeedc248dc0f5e91
Clément <clementdarthe@gmail.com>
2021-12-15 12:48:42 +0100

    Split

--- src/main.py
+++ src/main.py
@@ -3,20 +3,24 @@ import json
 with open("src/settings.json") as file:
             settings = json.load(file)
 
- ############################################ Imports ############################################
+######################################## HyperParameters ########################################
+CLASS_NAME = "class"
+DATA_NAME = "banknote"
+
+############################################ Imports ############################################
 import pandas as pd
 from preprocessing.cleaner import convert_cat_to_num,missing_value
 from preprocessing.test import test as p_test
 from preprocessing.label import label_encoder
-
+from preprocessing.split import split
 
 ############################################ Pipeline ############################################
 # Preprocessing
-data = pd.read_csv(settings["paths"]["banknote"])
+data = pd.read_csv(settings["paths"][DATA_NAME])
 clean_data = missing_value(data)
 p_test(clean_data,data)
 label_data,label_dico = label_encoder(data)
-
+X_train, X_test, y_train, y_test = split(data,CLASS_NAME)
 
 # Models
 
--- src/preprocessing/split.py
+++ src/preprocessing/split.py
@@ -1,8 +1,7 @@
 from sklearn.model_selection import ShuffleSplit, train_test_split
 
-def shuffle_split(data,n_splits,test_size=0.2,train_size=0.8) :
+def shuffle_split(n_splits,test_size=0.2,train_size=0.8) :
     """
-    data : Pandas dataframe
     n_splits : Integer, number of splits
     test_size : Float, representing the percentage of the test set
     train_size : Float, representing the percentage of the train set
@@ -13,10 +12,14 @@ def shuffle_split(data,n_splits,test_size=0.2,train_size=0.8) :
 
 
 
-def split(X,Y,test_size=0.2,train_size=0.8) :
+def split(data,target,test_size=0.2,train_size=0.8) :
     """
     X : numpy, data
     Y : numpy, target
     test_size : Float, representing the percentage of the test set
     """
+    if target not in data.columns :
+        raise ValueError
+    Y = data[target]
+    X = data.drop(data[target])
     return train_test_split(X,Y,test_size=0.33)
